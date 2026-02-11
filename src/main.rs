e85355ea89bd0bee82dc1cb7f3dc9171a85756ee
vango <viktorlilienberg@gmail.com>
2026-02-11 21:43:01 +0100

    Pull in logger crate and attach logger mdw

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use backend_core::configuration::get_configuration;
 use backend_core::startup::run;
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    //defaults info if RUST_LOG not set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
