c13c1991190cacec1b9e2b57072d6ecf86646f21
vango <viktorlilienberg@gmail.com>
2026-03-28 17:10:40 +0100

    Extract startup code from main.rs into startup.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use backend_core::email_client::EmailClient;
-use backend_core::startup::run;
+use backend_core::startup::build;
 use backend_core::telemetry::init_subscriber;
 use backend_core::{configuration::get_configuration, telemetry::get_subscriber};
-use sqlx::postgres::PgPoolOptions;
-
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -12,24 +8,9 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    let server = build(configuration).await?;
+    server.await?;
 
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
