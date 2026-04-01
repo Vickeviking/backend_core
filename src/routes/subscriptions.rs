3c96f844e8d10b52586a9e632826045251b1e3ec
vango <viktorlilienberg@gmail.com>
2026-04-01 21:34:39 +0200

    Making test green

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -90,16 +90,16 @@ pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
-    sqlx::query!(
+    sqlx::query(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
-        Uuid::new_v4(),
-        new_subscriber.email.as_ref(),
-        new_subscriber.name.as_ref(),
-        Utc::now()
     )
+    .bind(Uuid::new_v4())
+    .bind(new_subscriber.email.as_ref())
+    .bind(new_subscriber.name.as_ref())
+    .bind(Utc::now())
     .execute(pool)
     .await
     .map_err(|e| {
