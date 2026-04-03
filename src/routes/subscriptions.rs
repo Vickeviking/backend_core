6de5559477dac490fd22bf0fb657a07123eeb40d
vango <viktorlilienberg@gmail.com>
2026-04-03 15:17:21 +0200

    Clean linting errors

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use rand::distr::Alphanumeric;
-use rand::{rng, Rng};
+use rand::{Rng, rng};
 use sqlx::PgPool;
 use sqlx::{Executor, Postgres, Transaction};
 use unicode_segmentation::UnicodeSegmentation;
