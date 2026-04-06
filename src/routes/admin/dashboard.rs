8cc73cccc74a266fd85b6973ae3f258614effcff
vango <viktorlilienberg@gmail.com>
2026-04-06 22:36:45 +0200

    Protect admin routes

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,5 @@
-use actix_session::TypedSession;
-use actix_web::http::header::ContentType;
+use crate::session_state::TypedSession;
+use actix_web::http::header::{ContentType, LOCATION};
 use actix_web::{web, HttpResponse};
 use anyhow::Context;
 use sqlx::PgPool;
@@ -17,10 +17,12 @@ pub async fn admin_dashboard(
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let username = if let Some(user_id) = session.get_username().map_err(e500)? {
+    let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
         get_username(user_id, &pool).await.map_err(e500)?
     } else {
-        todo!()
+        return Ok(HttpResponse::SeeOther()
+            .insert_header((LOCATION, "/login"))
+            .finish());
     };
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
